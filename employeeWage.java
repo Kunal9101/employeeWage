6a19cf1a1e188886e15f64cfa8807bf4658964a4
Kunal <kunalmhatre9101@gmail.com>
2024-04-17 16:50:24 +0530

    printing wage for full & part time employees

--- employeeWage.java
+++ employeeWage.java
@@ -4,21 +4,28 @@ public class employeeWage {
     public static void main(String[] args) {
         //creating constats
         int RATE_PER_HOUR = 20;
+        int PART_TIME_HOUR = 4;
         int FULL_TIME_HOUR = 8;
 
         System.out.println("Welcome !!!");
 
         //create random function to check wheather employee is present or not
         Random rand = new Random();
-        boolean attendence = rand.nextBoolean();
+        float attendence = rand.nextFloat();
 
         //printing employee is present or not
         int empWage = 0;
-        if (attendence == true){
+        if (attendence >= 0.7){
             empWage = RATE_PER_HOUR * FULL_TIME_HOUR;
             System.out.println("Employee is present for full time...");
             System.out.println("Employee wage is "+ empWage);
-        } else {
+        } 
+        else if (attendence < 0.7 && attendence >=0.4){
+            empWage = RATE_PER_HOUR * PART_TIME_HOUR;
+            System.out.println("Employee is present for part time...");
+            System.out.println("Employee wage is "+ empWage);
+        }
+        else {
             System.out.println("Employee is Absent...");
             System.out.println("Employee wage is "+ empWage);
         }
