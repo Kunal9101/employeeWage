5aaeb3de51f53f62289ad5961c767b18d86cbd2a
Kunal <kunalmhatre9101@gmail.com>
2024-04-17 16:37:08 +0530

    creating random function and printing employee attendence

--- employeeWage.java
+++ employeeWage.java
@@ -1,5 +1,18 @@
+import java.util.*;;
+
 public class employeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome !!!");
+
+        //create random function to check wheather employee is present or not
+        Random rand = new Random();
+        boolean attendence = rand.nextBoolean();
+
+        //printing employee is present or not
+        if (attendence == true){
+            System.out.println("Employee is present...");
+        } else {
+            System.out.println("Employee is Absent...");
+        }
     }
 }
