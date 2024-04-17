2218f10f10c9bcd97aa92479a8612758596d8f71
Kunal <kunalmhatre9101@gmail.com>
2024-04-17 16:43:55 +0530

    printing wage for full time employees

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,10 @@ import java.util.*;;
 
 public class employeeWage {
     public static void main(String[] args) {
+        //creating constats
+        int RATE_PER_HOUR = 20;
+        int FULL_TIME_HOUR = 8;
+
         System.out.println("Welcome !!!");
 
         //create random function to check wheather employee is present or not
@@ -9,10 +13,14 @@ public class employeeWage {
         boolean attendence = rand.nextBoolean();
 
         //printing employee is present or not
+        int empWage = 0;
         if (attendence == true){
-            System.out.println("Employee is present...");
+            empWage = RATE_PER_HOUR * FULL_TIME_HOUR;
+            System.out.println("Employee is present for full time...");
+            System.out.println("Employee wage is "+ empWage);
         } else {
             System.out.println("Employee is Absent...");
+            System.out.println("Employee wage is "+ empWage);
         }
     }
 }
